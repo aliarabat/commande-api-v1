55b95f151723f22a586034e3522549888f739194
Ali <arabat50@gmail.com>
2019-03-03 20:47:30 +0100

    Jenkins commit

--- src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
+++ src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommandeApiV1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommandeApiV1Application.class, args);
+            SpringApplication.run(CommandeApiV1Application.class, args);
+            int n1=2;
+            String nom="";
+            System.out.println("Hello FST");
+            System.out.println("License sir");
 	}
 
 }
