7a266df430c0f65e11145d59494980856aef8db4
Ali <arabat50@gmail.com>
2019-03-03 22:22:04 +0100

    wamalek

--- src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
+++ src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
@@ -9,6 +9,7 @@ public class CommandeApiV1Application {
 	public static void main(String[] args) {
             SpringApplication.run(CommandeApiV1Application.class, args);
             int n1=4;
+            System.out.println("hello");
             String nom="";  
 	}
 
