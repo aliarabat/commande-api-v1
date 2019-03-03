4169f91527b9c1b3d52d59bbf5b2c572554e1ff6
Ali <arabat50@gmail.com>
2019-03-03 21:18:54 +0100

    second commit

--- src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
+++ src/main/java/com/fst/commandeapiv1/CommandeApiV1Application.java
@@ -8,12 +8,6 @@ public class CommandeApiV1Application {
 
 	public static void main(String[] args) {
             SpringApplication.run(CommandeApiV1Application.class, args);
-            int n1=2;
-            String nom="";
-            System.out.println("Hello FST");
-            System.out.println("License sir");
-            System.out.println("Bienvenue");
-            System.out.println("Bonjour");
 	}
 
 }
